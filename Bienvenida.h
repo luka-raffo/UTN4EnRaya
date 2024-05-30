53526e8871ba826c6081c370ef96fd7dba86aa27
guido-scotti <scottigui@gmail.com>
2024-05-29 23:43:31 -0300

    nuevo cartel de bienvenida, estilo maquina de escribir

--- Bienvenida.h
+++ Bienvenida.h
@@ -1,16 +1,101 @@
 #ifndef BIENVENIDA_H_INCLUDED
 #define BIENVENIDA_H_INCLUDED
 
-void mostrarAnimacionbienvenida()
 
+void mostrarAnimacionbienvenida()
 {
-    string primerCartel;
-    primerCartel="                                                 [-----BIENVENIDO A-----]";
-    cout<<primerCartel;
-    cout<<endl;
-    cout<<"                                               [-----| CUATRO EN RAYA |-----]"<<endl;
-    Sleep(2000);
-    system ("cls");
+    int i;
+    rlutil::hidecursor();
+    rlutil::setColor(rlutil::WHITE);
+    rlutil::locate(50, 13);
+    cout<<"C";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Cu";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Cua";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Cuat";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Cuatr";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Cuatro";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Cuatro ";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Cuatro e";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Cuatro en";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Cuatro en ";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Cuatro en R";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Cuatro en Ra";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Cuatro en Ray";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Cuatro en Raya";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Cuatro en Raya.";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Cuatro en Raya..";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Cuatro en Raya...";
+    Sleep(150);
+    system("cls");
+
+    for (i=1; i<3; i++) //FOR PARA QUE SE REPITA DOS VECES MAS
+    {
+        rlutil::locate(50, 13);
+        cout<<"Cuatro en Raya";
+        Sleep(150);
+        system("cls");
+        rlutil::locate(50, 13);
+        cout<<"Cuatro en Raya.";
+        Sleep(150);
+        system("cls");
+        rlutil::locate(50, 13);
+        cout<<"Cuatro en Raya..";
+        Sleep(150);
+        system("cls");
+        rlutil::locate(50, 13);
+        cout<<"Cuatro en Raya...";
+        Sleep(150);
+        system("cls");
+    }
+
 }
 
 
