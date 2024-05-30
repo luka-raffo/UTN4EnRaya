9fe6e4b1fb85cf0892edcf06f9a11e14d5f868b4
guido-scotti <scottigui@gmail.com>
2024-05-29 23:54:24 -0300

    menu adaptado al rlutil

--- MenuPrincipal.h
+++ MenuPrincipal.h
@@ -3,55 +3,69 @@
 
 bool mostrarMenuPrincipal (char matrizTablero[][7],int nfilas, int ncol)
 {
-
-
-
     char ingresoMenu;
-    cout<<"                                             [     CUATRO EN RAYA     ]"<<endl;
+    cout<<endl;
+    rlutil::locate(47, 2);
+    rlutil::setColor(rlutil::CYAN);
+    cout<<"[ -->  CUATRO EN RAYA  <-- ]"<<endl;
+    rlutil::setColor(rlutil::WHITE);
+    cout<<endl;
+    rlutil::locate(1, 4);
     cout<<"========================================================================================================================"<<endl;
-    cout<<"                                       [------- Seleccione una opcion -------] "<<endl;
+    cout<<endl;
+    rlutil::setColor(rlutil::CYAN);
+    rlutil::locate(42, 6);
+    cout<<"[------> Seleccione una opcion <------] "<<endl;
+    rlutil::setColor(rlutil::WHITE);
+    cout<<endl;
+    rlutil::locate(1, 8);
     cout<<"========================================================================================================================"<<endl;
-    cout<<"                                           -> Seleccione [1] para jugar <- "<<endl;
-    cout<<"                        -> Seleccione [2] para ver los objetivos y las reglas del juego <- "<<endl;
-    cout<<"                                           -> Seleccione [0] para salir <- "<<endl;
+    cout<<endl;
+    rlutil::locate(46, 10);
+    cout<<R<<"->"<<RESET<<" Seleccione [1] para jugar "<<R<<"<- "<<RESET<<endl;
+    rlutil::locate(29, 11);
+    cout<<R<<"->"<<RESET<<" Seleccione [2] para ver los objetivos y las reglas del juego "<<R<<"<- "<<RESET<<endl;
+    rlutil::locate(46, 12);
+    cout<<R<<"->"<<RESET<<" Seleccione [0] para salir "<<R<<"<- "<<RESET<<endl;
+    cout<<endl;
+    rlutil::locate(1, 14);
     cout<<"========================================================================================================================"<<endl;
-
-    cout<<"                                                Mi opcion es la - ";
+    cout<<endl;
+    rlutil::locate(59, 16);
+    cout<<"- ";
+    rlutil::locate(61, 16);
     cin>>ingresoMenu;
-
     system("cls");
 
     string jugador1;
     string jugador2;
-        switch(ingresoMenu)
+
+        switch(ingresoMenu) //INICIO DEL SWITCH DE OPCIONES
         {
-        case '1':
-
-
-            ///logica del juego
+        case '1': //INICIAR JUEGO
 
             system ("cls");
-            cargarMatrizCuadricula(matrizTablero, nfilas, ncol);
 
+            cargarMatrizCuadricula(matrizTablero, nfilas, ncol);
 
             logicaDelJuego(matrizTablero, nfilas, ncol);
 
-
             system ("cls");
+
             return 1;
 
             break;
 
-        case '2':
+        case '2': //MOSTRAR REGLAS Y OBJETIVOS
 
             mostrarReglas();
 
-            system ("pause");
+            volverAlMenu();
             system ("cls");
 
             break;
 
-        case '0':
+        case '0': //CERRAR EL JUEGO
             cierreDelJuego();
             return 0;
             break;
@@ -60,14 +74,10 @@ bool mostrarMenuPrincipal (char matrizTablero[][7],int nfilas, int ncol)
 
             ingresoIncorrecto();
 
-
             break;
         }
-
-
-
-
 
 }
 
 #endif // MENUPRINCIPAL_H_INCLUDED
+
